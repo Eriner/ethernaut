100d6c10bea5c5577da0e14a8ef2bbdcc517a295
Matt Hamilton <m@tthamilton.com>
2021-05-27 22:30:46 -0400

    cobra
    Signed-off-by: Matt Hamilton <m@tthamilton.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eriner/ethernaut
 
 go 1.16
 
-require github.com/ethereum/go-ethereum v1.10.3
+require (
+	github.com/ethereum/go-ethereum v1.10.3
+	github.com/spf13/cobra v0.0.3
+)
--- main.go
+++ main.go
@@ -1,42 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/spf13/cobra"
 )
 
-var password string
+const passwordEnvVar string = "ETHERNAUT_PASSWORD"
 
-func init() {
-	var ok bool
-	password, ok = os.LookupEnv("ETHERNAUT_PASSWORD")
-	if !ok {
-		log.Fatalln("ETHERNAUT_PASSWORD is unset")
+// it's either a global or an init() function. Pick your poision.
+var (
+	password string
+
+	client *ethclient.Client
+)
+
+func main() {
+	cmd := rootCommand()
+	if err := cmd.Execute(); err != nil {
+		log.Fatalln(err)
 	}
 }
 
-func main() {
-	client, err := ethclient.Dial("http://127.0.0.1:8545")
-	if err != nil {
-		log.Fatalln("Error connecting to ETH network:", err)
+func rootCommand() *cobra.Command {
+	rootCmd := &cobra.Command{
+		Use: "ethernaut",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			var ok bool
+			password, ok = os.LookupEnv(passwordEnvVar)
+			if !ok {
+				return fmt.Errorf(passwordEnvVar + "is unset")
+			}
+			if client != nil {
+				var err error
+				client, err = ethclient.Dial("http://127.0.0.1:8545")
+				if err != nil {
+					return fmt.Errorf("Error connecting to the ETH network: %w", err)
+				}
+			}
+			return nil
+		},
 	}
+	// flags := rootCmd.PersistentFlags()
+	return rootCmd
+}
 
+func getAccount() (*accounts.Account, error) {
 	ks := keystore.NewKeyStore("./wallets", keystore.StandardScryptN, keystore.StandardScryptP)
 	keystoreDat, err := ioutil.ReadFile("./wallets/ethernaut.dat")
 	if err != nil {
-		log.Fatalln("Error reading wallet file:", err)
+		return nil, fmt.Errorf("Error reading wallet file: %w", err)
 	}
 	account, err := ks.Import(keystoreDat, password, password)
 	if err != nil {
-		log.Fatalln("Error importing wallet:", err)
+		return nil, fmt.Errorf("Error importing wallet: %w", err)
 	}
-
-	address := account.Address.Hex()
-	log.Println("address:", address)
-
-	_ = client
+	return &account, nil
 }
